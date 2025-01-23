e35df1dd84dafdae3f9771c7cde100c15023aa13
jimbo23000 <antonio.ramos23@yahoo.com>
2025-01-22 18:39:41 -0600

    Layout and styling overhaul, officially upgraded the site to version 2.0

---
+++ collapsible.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const collapsible = document.querySelectorAll('.collapsible');
+    collapsible.forEach(button => {
+      button.addEventListener('click', function () {
+        this.classList.toggle('active');
+        const content = this.nextElementSibling;
+        content.style.display = content.style.display === 'block' ? 'none' : 'block';
+      });
+    });
+  });
